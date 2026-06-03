b6aa961171d38aead4eaa642f9dc01d248223f9f
Arsen Ibatullin <arsen.ibatullin21@gmail.com>
2026-06-03 16:40:30 +0500

    create admin views

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from main.models import Product, Platform, Genre, ProductImage
+
+
 # Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'discount', 'is_available']
+    list_editable = ['price']
+    inlines = [ProductImageInline]
+    prepopulated_fields = {'slug': ('name', )}
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+@admin.register(Genre)
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+
+
+
