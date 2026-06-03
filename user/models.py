67716b9768fab3a28997d34298882adc92efbad2
Arsen Ibatullin <arsen.ibatullin21@gmail.com>
2026-06-03 13:53:57 +0500

    User app and model creation

---
+++ user/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='profile_image/', default='profile_images/noimgage.jpg')
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    stripe_customer_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    is_subscribed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
